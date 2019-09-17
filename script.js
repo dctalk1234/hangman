a8609cb491bee8c1ce1128d5bf974706d78cb469
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-09-17 09:46:25 -0700

    fixing different branches

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+// let submitWordButton = document.querySelector('.submitWordButton');
+// let board = document.querySelector('.board');
+
+// let word;
+// function createBoard(e) {
+//     word = document.querySelector('.input').value;
+//     word = word.split('');
+
+//     for(let i = 0; i < word.length; i++)
+//     {
+//         let newLetter = document.createElement('p');
+//         newLetter.classList.add('letter');
+
+//         board.appendChild(newLetter);
+//     }
+
+//     let guessForm = document.createElement('input');
+//    guessForm.type = 'text';
+//    guessForm.placeholder = 'enter letter to guess';
+//    guessForm.classList.add('userGuess');
+//    board.appendChild(guessForm);
+
+//     let submitLetterGuessButton = document.createElement('input');
+//     submitLetterGuessButton.type = 'submit';
+//     submitLetterGuessButton.value = 'submit';
+//     submitLetterGuessButton.addEventListener('click', checkGuess);
+//     board.appendChild(submitLetterGuessButton);
+//     checkGuess();
+//     checkWin();
+// }
+
+// function checkGuess() {
+   
+//     guess = document.querySelector('.userGuess').value || " ";
+    
+    
+//     let letters = document.querySelectorAll('.letter');
+//     let correctGuess = false;
+//     for(let i = 0; i < word.length; i++)
+//     {
+//         if(guess.toLowerCase() === word[i].toLowerCase())
+//         {
+//             correctGuess = true;
+//             letters[i].innerText = guess;
+//             letters[i].style.borderBottom = 'none';
+          
+//         }
+//     }
+
+//     if(!correctGuess)
+//     {
+//         wrongGuess(guess);
+//     }
+// }
+
+// function wrongGuess(letter) {
+//     let strikes = document.querySelector('.strikes');
+//     let newStrike = document.createElement('div');
+//     newStrike.innerText = guess;
+//     strikes.appendChild(newStrike);
+// }
+
+// function checkWin() {
+    
+// }
+
+
+
+
+
+// submitWordButton.addEventListener('click', createBoard);
+
 let submitWordButton = document.querySelector('.submitWordButton');
 let board = document.querySelector('.board');
 
@@ -5,7 +77,7 @@ let word;
 function createBoard(e) {
     word = document.querySelector('.input').value;
     word = word.split('');
-
+    clearBoard();
     for(let i = 0; i < word.length; i++)
     {
         let newLetter = document.createElement('p');
@@ -26,7 +98,6 @@ function createBoard(e) {
     submitLetterGuessButton.addEventListener('click', checkGuess);
     board.appendChild(submitLetterGuessButton);
     checkGuess();
-    checkWin();
 }
 
 function checkGuess() {
@@ -35,33 +106,22 @@ function checkGuess() {
     
     
     let letters = document.querySelectorAll('.letter');
-    let correctGuess = false;
     for(let i = 0; i < word.length; i++)
     {
         if(guess.toLowerCase() === word[i].toLowerCase())
         {
-            correctGuess = true;
             letters[i].innerText = guess;
             letters[i].style.borderBottom = 'none';
           
         }
     }
+}
 
-    if(!correctGuess)
+function clearBoard() {
+    while(board.firstChild)
     {
-        wrongGuess(guess);
+        board.removeChild(board.firstChild);
     }
-}
-
-function wrongGuess(letter) {
-    let strikes = document.querySelector('.strikes');
-    let newStrike = document.createElement('div');
-    newStrike.innerText = guess;
-    strikes.appendChild(newStrike);
-}
-
-function checkWin() {
-    
 }
 
 
