9cd3ece65cc270b590cc2e0e6775ef8d7d351ce8
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-09-16 17:35:16 -0700

    most base js working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let submitWordButton = document.querySelector('.submitWordButton');
 let board = document.querySelector('.board');
 
-
+let word;
 function createBoard(e) {
-    let word = document.querySelector('.input').value;
+    word = document.querySelector('.input').value;
     word = word.split('');
 
     for(let i = 0; i < word.length; i++)
@@ -17,14 +17,38 @@ function createBoard(e) {
     let guessForm = document.createElement('input');
    guessForm.type = 'text';
    guessForm.placeholder = 'enter letter to guess';
+   guessForm.classList.add('userGuess');
    board.appendChild(guessForm);
 
     let submitLetterGuessButton = document.createElement('input');
     submitLetterGuessButton.type = 'submit';
     submitLetterGuessButton.value = 'submit';
+    submitLetterGuessButton.addEventListener('click', checkGuess);
     board.appendChild(submitLetterGuessButton);
-
-    
+    checkGuess();
 }
+
+function checkGuess() {
+   
+    guess = document.querySelector('.userGuess').value || " ";
+    
+    
+    let letters = document.querySelectorAll('.letter');
+    for(let i = 0; i < word.length; i++)
+    {
+        if(guess.toLowerCase() === word[i].toLowerCase())
+        {
+            letters[i].innerText = guess;
+            letters[i].style.borderBottom = 'none';
+          
+        }
+    }
+}
+
+
+
+
+
+
 
 submitWordButton.addEventListener('click', createBoard);
